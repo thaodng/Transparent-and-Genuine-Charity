cc8f20821621ad3080d4e0056012c22166c252df
Duy Nguyen <ngduythao2805@outlook.com>
2020-06-16 10:53:08 +0700

    Improve layout

--- client/src/pages/CharityList.js
+++ client/src/pages/CharityList.js
@@ -1,16 +1,24 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { DropdownButton, Dropdown } from 'react-bootstrap';
+import axios from 'axios';
+import { DropdownButton, Dropdown, Spinner } from 'react-bootstrap';
 import { CharityContext } from '../context/CharityContext';
 import Charity from '../components/Charity';
+import { PUBLIC_API_URL } from '../apis/config';
 
 const CharityList = () => {
-  const { charities } = useContext(CharityContext);
+  const { charities, setCharities } = useContext(CharityContext);
+
   const [searchTerm, setSearchTerm] = useState('');
   const [filterCharities, setFilterCharities] = useState([]);
 
   useEffect(() => {
-    setFilterCharities(charities);
-  }, [charities]);
+    const getCharities = async () => {
+      const { data: { charitySearchResults } } = await axios.get(PUBLIC_API_URL);
+      setCharities(charitySearchResults);
+      setFilterCharities(charitySearchResults);
+    };
+    getCharities();
+  }, [setCharities]);
 
   const onSearch = (e) => {
     setSearchTerm(e.target.value);
@@ -32,67 +40,70 @@ const CharityList = () => {
   };
 
   return (
-    <div>
-      <div>
-        <h1 className="p-4 display-4 text-center"> Our Charity Partners </h1>
-      </div>
-
-      <div className="container">
-
-        {/* 
-        <div className="d-flex flex-row justity-content-end">
-          <button type="button" class="btn btn-outline-primary">Create donate</button>
-        </div> 
-        */}
-
-        <div className="text-right mb-2">
-          <button type="button" class="btn btn-outline-primary">Create donate</button>
-        </div>
-
-        <div className="row">
-          <div className="col-md-2">
-            <DropdownButton
-              id="dropdown-1"
-              title={selectedOption}
-              variant="primary active"
-              onSelect={onSelect}
-              alignRight
-            >
-              {
-                options.map((opt, i) => <Dropdown.Item key={i} eventKey={i}>{opt}</Dropdown.Item>)
-              }
-            </DropdownButton>
+    <>
+      {
+        filterCharities.length === 0
+          ?
+          <div className="d-flex justify-content-center text-center">
+            <Spinner animation="border" role="status">
+            </Spinner>
           </div>
-          <div className="col-md-10 pl-4">
-            <form className='form-group'>
-              <input
-                className='form-control form-control'
-                type='text'
-                placeholder='Search'
-                value={searchTerm}
-                onChange={(e) => onSearch(e)}
-              />
-            </form>
-          </div>
-        </div>
+          :
+          <>
+            <div>
+              <h1 className="p-4 display-4 text-center"> Our Charity Partners </h1>
+            </div>
 
-        <div className="row">
-          {
-            filterCharities.map(({ charityId, charityDisplayName, description, logoUrl, registrationNumber }) =>
-              <Charity
-                key={charityId}
-                charityId={charityId}
-                charityDisplayName={charityDisplayName}
-                description={description}
-                logoUrl={logoUrl}
-                registrationNumber={registrationNumber}
-              />
-            )
-          }
-        </div>
-      </div>
+            <div className="container">
+              <div className="text-right mb-2">
+                <button type="button" class="btn btn-outline-primary">Create donate</button>
+              </div>
 
-    </div>
+              <div className="row">
+                <div className="col-md-2">
+                  <DropdownButton
+                    id="dropdown-1"
+                    title={selectedOption}
+                    variant="primary active"
+                    onSelect={onSelect}
+                    alignRight
+                  >
+                    {
+                      options.map((opt, i) => <Dropdown.Item key={i} eventKey={i}>{opt}</Dropdown.Item>)
+                    }
+                  </DropdownButton>
+                </div>
+                <div className="col-md-10 pl-4">
+                  <form className='form-group'>
+                    <input
+                      className='form-control form-control'
+                      type='text'
+                      placeholder='Search'
+                      value={searchTerm}
+                      onChange={(e) => onSearch(e)}
+                    />
+                  </form>
+                </div>
+              </div>
+
+              <div className="row">
+                {
+                  filterCharities.map(({ charityId, charityDisplayName, description, logoUrl, registrationNumber }) =>
+                    <Charity
+                      key={charityId}
+                      charityId={charityId}
+                      charityDisplayName={charityDisplayName}
+                      description={description}
+                      logoUrl={logoUrl}
+                      registrationNumber={registrationNumber}
+                    />
+                  )
+                }
+              </div>
+            </div>
+          </>
+      }
+    </>
   )
 }
 
