69115c4d54665a8d3545add0ea61d6412cba85dc
Duy Nguyen <ngduythao2805@outlook.com>
2020-06-18 10:51:08 +0700

    Update charity abi

--- client/src/apis/config.js
+++ client/src/apis/config.js
@@ -1,4 +1,4 @@
-const FACTORY_ADDRESS ='0xcd88bC069DEdD4972498f985C667cad2618Fc749';
+const FACTORY_ADDRESS ='0x0C45b306625D2CeC3400ab611e7c3d44D335aC14';
 const PUBLIC_API_URL = 'https://api.justgiving.com/2605491e/v2/charity/search';
 const LOGO_URL = 'https://www.pngkit.com/png/detail/246-2467534_your-logo-here-png-your-brand-here-png.png';
 const MY_API_URL = 'http://localhost:5000';
