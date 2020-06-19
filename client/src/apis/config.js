c7927661d6cde2383375caf086f3190e0b2ab778
Duy Nguyen <ngduythao2805@outlook.com>
2020-06-19 22:19:42 +0700

    Update factory address

--- client/src/apis/config.js
+++ client/src/apis/config.js
@@ -1,4 +1,4 @@
-const FACTORY_ADDRESS ='0x0C45b306625D2CeC3400ab611e7c3d44D335aC14';
+const FACTORY_ADDRESS ='0xE897D9F9be8B598718ab93Fd65D86617ddbE769a';
 const PUBLIC_API_URL = 'https://api.justgiving.com/2605491e/v2/charity/search';
 
 const MY_API = 'http://localhost:5000';
