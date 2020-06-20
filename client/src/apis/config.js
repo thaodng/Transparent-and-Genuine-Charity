c13c6a0efe55b9b4894cd466c843b9e71c87cc72
Duy Nguyen <ngduythao2805@outlook.com>
2020-06-20 09:06:25 +0700

    Rest in peace...

--- client/src/apis/config.js
+++ client/src/apis/config.js
@@ -1,4 +1,4 @@
-const FACTORY_ADDRESS ='0xE897D9F9be8B598718ab93Fd65D86617ddbE769a';
+const FACTORY_ADDRESS ='0xCBeC0f8e0FaA52f500E5dA58D281D908EF59890F';
 const PUBLIC_API_URL = 'https://api.justgiving.com/2605491e/v2/charity/search';
 
 const MY_API = 'http://localhost:5000';
